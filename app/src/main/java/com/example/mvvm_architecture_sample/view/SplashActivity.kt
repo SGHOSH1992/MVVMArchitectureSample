1dd4fc7bf37ec4e7413f069d358cc466b62439c2
Sudip Ghosh <sudipghosh682@gmail.com>
2022-06-10 00:28:08 +0530

    Splash Screen Update 10-06-2022

--- app/src/main/java/com/example/mvvm_architecture_sample/view/SplashActivity.kt
+++ app/src/main/java/com/example/mvvm_architecture_sample/view/SplashActivity.kt
@@ -2,11 +2,27 @@ package com.example.mvvm_architecture_sample.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.mvvm_architecture_sample.R
 
 class SplashActivity : AppCompatActivity() {
+    var isLoading : Boolean = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Handler(Looper.getMainLooper()).postDelayed({
+            isLoading=false
+        },5000)
+
+
+        installSplashScreen().apply {
+            setKeepOnScreenCondition{
+                isLoading
+            }
+        }
         setContentView(R.layout.activity_splash)
+
     }
 }
